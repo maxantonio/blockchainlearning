65dc377399ca8668171690e765d4028c2954d6a2
Yoidel Martinez <yoidel86@gmail.com>
2021-11-12 08:58:00 -0500

    agregando desconeccion y actualizando al conectar

--- exercises/react-web3/04-conectando-metamask/src/App.js
+++ exercises/react-web3/04-conectando-metamask/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import { Container } from 'react-bootstrap';
 import Web3 from "web3";
-
+var that = {};
 
 class App extends React.Component {
   constructor(props) {
@@ -12,18 +12,32 @@ class App extends React.Component {
       balance: '',
       net:'',
       isAddress: false,
-      loading: false
+      loading: false,
+      status:'Conectar'
     };
     /** 
      * Iniciando objeto Web3 
      * @example this.web3 = new Web3('https://data-seed-prebsc-1-s1.binance.org:8545');
      */
     this.web3 = new Web3(process.env.REACT_APP_PROVIDER_URL);
+    var storag = window.localStorage.getItem('connected');
+    if(storag){
+     window.ethereum.request({ method: 'eth_accounts' }).then(a=>this.setStatus(a));
+    }
+    that = this;
+  }
+  setStatus(a){
+      if(a.length>0){
+        console.log("conectado si esta",a);
+        this.setState({status:'Conectado',address:a[0]});
+        
+      }
   }
   setWalletAddress (wallet){
     console.log(wallet);
+    window.localStorage.setItem("connected",true);
     if(wallet)
-      this.setState({address:wallet});
+      this.setState({address:wallet,status:'Conectado'});
     else
     this.setState({address:"connect your account and try again to show"});
   }
@@ -60,6 +74,25 @@ class App extends React.Component {
       alert("Please install MetaMask");
     }
   }
+   disconnectWallet(){
+     window.ethereum.request({
+      method: "wallet_requestPermissions",
+      params: [
+        {
+          eth_accounts: {}
+        }
+      ]
+    }).then(function(a){
+      console.log("connected ",a);
+      window.localStorage.setItem("connected",true);
+      that.connectWallet()
+    });
+  }
+ 
+  async disconnectWalletS(){
+   window.localStorage.removeItem('connected');
+   that.setState({status:"Conectar",address:""})
+  }
 
   
 
@@ -68,10 +101,13 @@ class App extends React.Component {
   render() {
     return (
       <Container className="d-flex justify-content-center align-items-center vh-100">
-         <div>  
-         <button className="btn btn-light text-dark" onClick={this.connectWallet}>Connect</button>
-         <div className="address">Address: {this.state.address}</div>
-         <div className="address">Network: {this.state.chainId}</div>
+         <div className="text-center">  
+         <button className="btn btn-light text-dark" onClick={this.disconnectWallet}>{this.state.status}</button>
+         { this.state.status == 'Conectado' &&
+          <button className="btn btn-light text-dark ml-2" onClick={this.disconnectWalletS}>Desconectar</button>
+         }
+         <div className="card p-2"> {this.state.address}</div>
+         
          </div>
       </Container>
     );
